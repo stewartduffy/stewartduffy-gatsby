1d03f5832bf3c73d54ca4ef8811786417b16fa12
Stewart Duffy <duffy.stewart@gmail.com>
2018-03-31 20:13:47 +1300

    Updates content block

--- src/components/ContentBlock/ContentBlock.js
+++ src/components/ContentBlock/ContentBlock.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import { Icon } from '../Icon'
+import styles from './ContentBlock.module.scss'
 
-const ContentBlock = ({ data, icon }) => {
-  return (
-    <div className="row">
-      <div className="col-md-1 hidden-sm hidden-xs">
-        <i className={`main-icon fa ${icon}`} />
-      </div>
-      <div
-        className="col-md-11"
-        dangerouslySetInnerHTML={{
-          __html: data.childMarkdownRemark.html,
-        }}
-      />
+const ContentBlock = ({ data, icon, slug }) => (
+  <div className={`row ${styles['content-block']}`} id={slug}>
+    <div className="col-md-1 hidden-sm hidden-xs">
+      <Icon name={icon} />
     </div>
-  )
-}
+    <div
+      className={`col-md-11 ${styles['content-block-html']}`}
+      dangerouslySetInnerHTML={{
+        __html: data.childMarkdownRemark.html,
+      }}
+    />
+  </div>
+)
 
 export default ContentBlock
