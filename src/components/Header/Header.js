7944e8aef3046bdbc4ddd04dfe368f00d06b8742
Stewart Duffy <duffy.stewart@gmail.com>
2018-04-03 06:14:34 +1200

    Adds Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import Link, { navigateTo } from 'gatsby-link'
+import { Nav } from '../Nav'
+import logo from '../../img/logo.svg'
+import styles from './Header.module.scss'
+
+const Header = ({ data, location }) => (
+  <div className={styles['header']}>
+    <img
+      className={styles['heading-logo']}
+      src={logo}
+      alt="Stewart Duffy / Front-End Developer"
+    />
+    <Nav />
+  </div>
+)
+
+export default Header
