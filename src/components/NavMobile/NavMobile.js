b42f721361ec5ac6e7e1eae6a4f6426b5491ae57
Stewart Duffy <duffy.stewart@gmail.com>
2018-04-06 17:55:07 +1200

    Adds esc key handler to MobileNab

--- src/components/NavMobile/NavMobile.js
+++ src/components/NavMobile/NavMobile.js
@@ -5,32 +5,53 @@ import { NavLink } from '../NavLink'
 import { NavIcon } from '../NavIcon'
 import styles from './NavMobile.module.scss'
 
-const NavMobile = ({ handleMenuClick }) => {
-  return ReactDOM.createPortal(
-    <React.Fragment>
-      <nav className={`${styles['c-navigation']}`} role="navigation">
-        <div>
-          <NavIcon iconClass="times" onClick={handleMenuClick} />
-        </div>
-        <ul className={`${styles['c-navigation__list']}`}>
-          <li>
-            <NavLink text="What I Do" href="#what-i-do" />
-          </li>
-          <li>
-            <NavLink text="Work" href="#work-history" />
-          </li>
-          <li>
-            <NavLink text="Contact" href="#lets-talk" />
-          </li>
-          <li>
-            <NavLink text="CV" href={'#cv'} />
-          </li>
-        </ul>
-      </nav>
-      <div className={styles['c-overlay']} />
-    </React.Fragment>,
-    document.getElementById('___gatsby')
-  )
+const NavMobile = class extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.escFunction = this.escFunction.bind(this)
+  }
+
+  escFunction(event) {
+    if (event.keyCode === 27) {
+      this.props.handleMenuClick()
+    }
+  }
+  componentDidMount() {
+    document.addEventListener('keydown', this.escFunction, false)
+  }
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.escFunction, false)
+  }
+
+  render() {
+    const { handleMenuClick } = this.props
+    return ReactDOM.createPortal(
+      <React.Fragment>
+        <nav className={`${styles['c-navigation']}`} role="navigation">
+          <div>
+            <NavIcon iconClass="times" onClick={handleMenuClick} />
+          </div>
+          <ul className={`${styles['c-navigation__list']}`}>
+            <li>
+              <NavLink text="What I Do" href="#what-i-do" />
+            </li>
+            <li>
+              <NavLink text="Work" href="#work-history" />
+            </li>
+            <li>
+              <NavLink text="Contact" href="#lets-talk" />
+            </li>
+            <li>
+              <NavLink text="CV" href={'#cv'} />
+            </li>
+          </ul>
+        </nav>
+        <div className={styles['c-overlay']} />
+      </React.Fragment>,
+      document.getElementById('___gatsby')
+    )
+  }
 }
 
 export default NavMobile
