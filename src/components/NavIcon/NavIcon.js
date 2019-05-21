a9f3e692c64b12d7e0feec73250c27bd102df3ce
Stewart Duffy <duffy.stewart@gmail.com>
2019-05-22 08:41:53 +1200

    Updates NavIcon to use styledComponents

--- src/components/NavIcon/NavIcon.js
+++ src/components/NavIcon/NavIcon.js
@@ -1,26 +1,85 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import Link, { navigateTo } from 'gatsby-link'
-
+import React from 'react'
+import styled from 'styled-components'
 import { Icon } from '../Icon'
+import * as theme from '../../config/theme'
 
-import styles from './NavIcon.module.scss'
+const StyledNavIconLink = styled.a`
+  display: flex;
+  align-items: center;
+  height: 53px;
+  padding: 5px;
+
+  svg {
+    width: 24px;
+    height: auto;
+
+    path {
+      fill: ${theme.themeWhite};
+    }
+
+    &:hover {
+      path {
+        fill: ${theme.themeGreen};
+      }
+    }
+  }
+`
+
+//@TODO: Find a better way to share styles cross element
+const StyledNavIconButton = styled.button`
+  display: flex;
+  align-items: center;
+  height: 53px;
+  padding: 5px;
+
+  display: inline-block;
+  margin-bottom: 0;
+  text-align: center;
+  vertical-align: middle;
+  touch-action: manipulation;
+  cursor: pointer;
+  background-image: none;
+  border: 1px solid transparent;
+  white-space: nowrap;
+  padding: 6px 12px;
+  font-size: 14px;
+  line-height: 1.42857;
+  user-select: none;
+  color: #337ab7;
+  font-weight: 400;
+  border-radius: 0;
+  box-shadow: none;
+  background-color: transparent;
+  border-color: transparent;
+
+  svg {
+    width: 24px;
+    height: auto;
+
+    path {
+      fill: ${theme.themeWhite};
+    }
+
+    &:hover {
+      path {
+        fill: ${theme.themeGreen};
+      }
+    }
+  }
+`
 
 const NavIcon = ({ iconClass, href, onClick }) => {
   if (href) {
     return (
-      <a className={styles['nav-icon']} href={href}>
+      <StyledNavIconLink href={href}>
         <Icon name={iconClass} />
-      </a>
+      </StyledNavIconLink>
     )
   } else {
     return (
-      <button
-        className={`btn btn-link ${styles['nav-icon']}`}
-        onClick={onClick}
-      >
+      <StyledNavIconButton className={`btn btn-link`} type="button" onClick={onClick}>
         <Icon name={iconClass} />
-      </button>
+      </StyledNavIconButton>
     )
   }
 }
