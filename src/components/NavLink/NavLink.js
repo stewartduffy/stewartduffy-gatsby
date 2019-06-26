5f80dfaae9130bf327436bbab414ff2bfc39f9c6
Stewart Duffy <duffy.stewart@gmail.com>
2019-06-27 07:45:26 +1200

    NavLink

--- src/components/NavLink/NavLink.js
+++ src/components/NavLink/NavLink.js
@@ -1,12 +1,29 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import Link, { navigateTo } from 'gatsby-link'
-import styles from './NavLink.module.scss'
+import React from 'react'
+import styled from 'styled-components'
+import { breakpoints, theme } from '../../config'
+
+//@TODO: Fix .active state
+const StyledNavLink = styled.a`
+  color: ${theme.themeWhite};
+  font-size: 20px;
+  font-family: ${theme.themeSansSerif};
+  transition: font-size 0.3s ease-out;
+
+  &:hover,
+  &.active {
+    text-decoration: none;
+    color: ${theme.themeGreen};
+  }
+
+  ${breakpoints.up('l')`
+    font-size: 22px;
+  `}
+`
 
 const NavLink = ({ text, href, onClick }) => (
-  <a className={styles['nav-item-link']} href={href} onClick={onClick}>
+  <StyledNavLink href={href} onClick={onClick}>
     {text}
-  </a>
+  </StyledNavLink>
 )
 
 export default NavLink
