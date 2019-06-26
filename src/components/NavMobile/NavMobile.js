43d3e7264b9d0ce728286959a1ddc8627d88df8c
Stewart Duffy <duffy.stewart@gmail.com>
2019-06-27 08:39:19 +1200

    Removes NavMobile scss

--- src/components/NavMobile/NavMobile.js
+++ src/components/NavMobile/NavMobile.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import ReactDOM from 'react-dom'
 import { NavLink } from '../NavLink'
 import { NavIcon } from '../NavIcon'
-import styles from './NavMobile.module.scss'
 import cv from '../../../static/assets/stewart_duffy_cv.pdf'
+import styled from 'styled-components'
+import { theme } from '../../config'
 
 const links = [
   {
@@ -28,6 +29,63 @@ const links = [
   },
 ]
 
+const StyledNavMobile = styled.nav`
+  background: #323232;
+  position: fixed;
+  top: 20px;
+  right: 20px;
+  max-width: calc(100% - 40px);
+  max-height: calc(100% - 40px);
+  z-index: ${theme.zIndex_5};
+  box-shadow: 0 2px 0 rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+  border-radius: 4px;
+`
+
+//@TODO: Give this a class
+const NavMobileClose = styled.div`
+  text-align: right;
+  position: absolute;
+  top: 0;
+  left: auto;
+  right: 0;
+`
+
+const NavMobileList = styled.ul`
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
+  padding: 0;
+
+  li {
+    font-size: 20px;
+    text-align: center;
+    font-weight: 600;
+  }
+
+  a {
+    padding: 10px;
+    display: block;
+  }
+`
+
+const NavMobileOverlay = styled.div`
+  pointer-events: auto;
+  opacity: 0.5;
+  height: 100%;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: $z-index-4;
+  background-color: ${theme.themeBlack};
+  pointer-events: none;
+`
+
 const NavMobile = class extends React.Component {
   escFunction = event => {
     if (event.keyCode === 27) {
@@ -50,21 +108,21 @@ const NavMobile = class extends React.Component {
   render() {
     const { handleMenuClick } = this.props
     return ReactDOM.createPortal(
-      <React.Fragment>
-        <nav className={`${styles['nav-mobile']}`} role="navigation">
-          <div className={`${styles['nav-mobile-close']}`}>
+      <Fragment>
+        <StyledNavMobile role="navigation">
+          <NavMobileClose>
             <NavIcon iconClass="times" onClick={handleMenuClick} />
-          </div>
-          <ul className={`${styles['nav-mobile-list']}`}>
+          </NavMobileClose>
+          <NavMobileList>
             {links.map(({ text, href }, id) => (
               <li key={`MobileNavLink-${id}`}>
                 <NavLink text={text} href={href} onClick={this.onNavLinkClick} />
               </li>
             ))}
-          </ul>
-        </nav>
-        <div className={styles['nav-mobile-overlay']} />
-      </React.Fragment>,
+          </NavMobileList>
+        </StyledNavMobile>
+        <NavMobileOverlay />
+      </Fragment>,
       document.getElementById('___gatsby')
     )
   }
